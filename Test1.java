a16f3bee119fbfae1eefdf10e6692e8216c909ba
iris94999 <iris94999@126.com>
2017-02-21 14:46:59 +0800

    test1

---
+++ Test1.java
@@ -0,0 +1,13 @@
+package Test;
+
+public class Test1 {
+  public static void main(String[] args) {
+    System.out.println("What's wrong with this program?");
+  }
+}
+ class TestAnother1 {
+  public static void main(String[] args) {
+    System.out.println("What's wrong with this program?");
+  }
+}
+//因为一个.java文件里面只能有一个public(主类)
